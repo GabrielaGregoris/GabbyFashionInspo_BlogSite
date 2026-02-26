828628f718394a3e0467df6ec9880d5eb629b326
GabrielaGregoris <n01434950@humbermail.ca>
2026-02-26 11:31:14 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Gabby Fashion Inspo Blog Site Loaded');
+
+    // Future functionality for mobile menu or interaction can be added here
+});
